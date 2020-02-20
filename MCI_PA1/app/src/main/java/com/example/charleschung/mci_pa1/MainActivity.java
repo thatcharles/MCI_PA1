28c7f7ba74a3f22cc64ad7247fb875407e6628e0
Charles Chung <charles5j21z@gmail.com>
2020-02-19 22:16:34 -0500

    add sensor data

--- MCI_PA1/app/src/main/java/com/example/charleschung/mci_pa1/MainActivity.java
+++ MCI_PA1/app/src/main/java/com/example/charleschung/mci_pa1/MainActivity.java
@@ -5,10 +5,19 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -17,6 +26,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager sensorManager;
     private Sensor Accelerometer;
+    private Sensor Gyroscope;
+    private Sensor Magnetic;
+    private Sensor Light;
+    private Sensor Borameter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +38,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Log.d(TAG, "onCreate: Initializing sensor services");
 
+        Button BtnStart = (Button) findViewById(R.id.button_start);
+        Button BtnStop = (Button) findViewById(R.id.button_stop);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Accelerometer = null;
+        Gyroscope = null;
+        Magnetic = null;
+        Light = null;
+        Borameter = null;
 
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
-
             Accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.registerListener(this, Accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         }
-        if (Accelerometer == null){
-            // Use the accelerometer.
-            if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
-                Accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            } else{
-                // Sorry, there are no accelerometers on your device.
-                // You can't play this game.
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+            Gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        }
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+            Magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        }
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+            Light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        }
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+            Borameter = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        }
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                sensorManager.registerListener(MainActivity.this, Accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+                sensorManager.registerListener(MainActivity.this, Gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+                sensorManager.registerListener(MainActivity.this, Magnetic, SensorManager.SENSOR_DELAY_FASTEST);
+                sensorManager.registerListener(MainActivity.this, Light, SensorManager.SENSOR_DELAY_FASTEST);
+                sensorManager.registerListener(MainActivity.this, Borameter, SensorManager.SENSOR_DELAY_FASTEST);
             }
-        }
+        });
+
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                sensorManager.unregisterListener(MainActivity.this);
+            }
+        });
 
         Log.d(TAG, "onCreate: Registered accelerometer listener.");
 
@@ -49,15 +91,45 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
+        Sensor sensor = event.sensor;
         float[] value = event.values;
-        // Do something with this sensor value.
-        Log.d(TAG, "onSensorChanged: X:" + value[0] + ",Y: " + value[1] + ",Z: " + value[2]);
+
+        float currentX = 0;
+        float currentY = 0;
+        float currentZ = 0;
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            currentX = value[0];
+            currentY = value[1];
+            currentZ = value[2];
+            Log.d(TAG, "onSensorChanged: X:" + value[0] + ",Y: " + value[1] + ",Z: " + value[2]);
+        }
+        if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            Log.d(TAG, "onSensorChanged GYROSCOPE:" + value.length);
+        }
+        if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            Log.d(TAG, "onSensorChanged MAGNETIC FIELD:" + value[0]);
+        }
+        if (sensor.getType() == Sensor.TYPE_LIGHT) {
+            Log.d(TAG, "onSensorChanged LIGHT:" + value[0]);
+        }
+
+        String entry = currentX + "," + currentY + "," + currentZ;
+        writeToFile (entry, this);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-        
+    }
+
+    private void writeToFile(String entry, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.csv", Context.MODE_PRIVATE));
+            outputStreamWriter.write(entry);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
